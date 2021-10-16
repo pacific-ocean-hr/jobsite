754ecd08a4b33af73156951647193abd3b468c5b
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-15 19:50:03 -0700

    setup routes

--- jobs/index.js
+++ jobs/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Jay!')
 })
+
+app.get('/api/jobs', (req, res) => {
+  // GET https://remotive.io/api/remote-jobs
+  // save to db
+
+  res.send('jobs');
+});
+
+app.get('/api/jobs/:id', (req, res) => {
+  // we may or may not need this
+  res.send('job');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
