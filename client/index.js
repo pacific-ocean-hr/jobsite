ba2ca440b914f97e7ecff3170523e5c82662e777
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-18 16:32:21 -0700

    Add ability to view routes without link

--- client/index.js
+++ client/index.js
@@ -8,6 +8,14 @@ app.use(express.static(path.join(__dirname, '/dist')));
 
 app.use(express.json());
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/index.html'), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
