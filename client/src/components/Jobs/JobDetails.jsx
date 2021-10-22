9c1e25722fb79bd3a13cf2bc4ef2325b8fe7816b
cschuhmacher <chris.r.schuhmacher@gmail.com>
2021-10-21 23:41:16 -0700

    style favorite icon

--- client/src/components/Jobs/JobDetails.jsx
+++ client/src/components/Jobs/JobDetails.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { BsHeart } from 'react-icons/bs';
 import { FaHeart } from 'react-icons/fa';
 
-const JobDetails = ({ job }) => {
-  const [heartColor, setHeartColor] = useState('white');
 
-  const onHeartClick = () => {
-    const color = heartColor === 'white' ? 'pink' : 'white';
-    setHeartColor(color);
-  };
+const ListingHeader = styled.div`
+  align-items: center;
+  display: flex;
+  h2 {
+    flex: 1;
+  }
+`;
+
+const JobDetails = ({ job }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  // const onHeartClick = () => {
+  //   const color = heartColor === 'white' ? 'pink' : 'white';
+  //   set(color);
+  // };
 
   return (
     <div>
-      <h2>
-        {job.title}
-        <FaHeart
-          fill={heartColor}
-          stroke="black"
-          strokeWidth="20px"
-          style={{ cursor: 'pointer' }}
-          onClick={onHeartClick}
-        />
-      </h2>
+      <ListingHeader>
+        <h2>{job.title}</h2>
+        {isFavorite && <FaHeart color="pink" size={24} onClick={() => setIsFavorite(false)} />}
+        {!isFavorite && <BsHeart color="#49475B" size={24} onClick={() => setIsFavorite(true)} />}
+      </ListingHeader>
       <div
         dangerouslySetInnerHTML={{
           __html: job.description,
